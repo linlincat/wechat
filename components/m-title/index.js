f3adad5271e6c7b524494c11fcb219738eae309a
cat <none>
2023-03-22 13:26:05 +0800

    标题组件封装

---
+++ components/m-title/index.js
@@ -0,0 +1,28 @@
+// components/m-title/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: String,
+    rightText: String,
+    showArray: {
+      type: Boolean,
+      value: false
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
